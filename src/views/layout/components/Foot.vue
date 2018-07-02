2346637056343c3b6624a541047c78037e1481ac
JohnnyCold <40761260+JohnnyCold@users.noreply.github.com>
2018-07-02 21:16:07 +0800

    Update Foot.vue

--- src/views/layout/components/Foot.vue
+++ src/views/layout/components/Foot.vue
@@ -3,9 +3,9 @@
 		<el-row>
 			<el-col :span="10">
 				<div>
-					© 2018 GitHub-Laziji&emsp;&emsp;
-					<a href="https://github.com/GitHub-Laziji" target="_blank">Profile</a>&emsp;&emsp;
-					<a href="https://github.com/GitHub-Laziji/vblog" target="_blank">VBlog</a>
+					© 2018 JohnnyCold&emsp;&emsp;
+					<a href="https://github.com/JohnnyCold" target="_blank">Profile</a>&emsp;&emsp;
+					<a href="https://github.com/JohnnyCold/vblog" target="_blank">VBlog</a>
 				</div>
 			</el-col>
 			<el-col :span="4">
